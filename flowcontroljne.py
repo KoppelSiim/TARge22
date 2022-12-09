33a22dc51e73545e4b4679cbadb78b2e09d2e2aa
Siim Koppel <siimk999@gmail.com>
2022-12-09 16:57:48 +0200

    flow

---
+++ flowcontroljne.py
@@ -0,0 +1,17 @@
+
+
+def main():
+    tervita()
+    misnimi()
+
+def tervita():
+    print("Tere maailm")
+
+def misnimi():
+    nimi = input("Mis on Sinu nimi?\n")
+    print("Tere " + nimi)
+    print(3 ** 2)
+
+    
+if __name__ == '__main__':
+    main()
