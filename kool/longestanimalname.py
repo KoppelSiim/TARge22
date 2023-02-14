b99361d3774d9af06a0c3c37de1948ca697b20d0
Siim Koppel <siimk999@gmail.com>
2023-02-14 10:39:21 +0200

    longest animalname, fixed a bug

--- kool/longestanimalname.py
+++ kool/longestanimalname.py
@@ -1,5 +1,4 @@
 """
-
 Kirjuta programm:
 1. Küsi looma nimesid kuni tühja sisestuseni
 2. Kuva sama tähega algavate loomade kõige pikemad nimed
@@ -11,9 +10,6 @@ Tulem: K – Koaala S – Saarmas R – Rebane
 
 animals = []
 letters = []
-first_letter = ""
-longest_word = ""
-longest_word_len = 0
 
 while True:
     # capitalize the first letter
@@ -27,13 +23,15 @@ while True:
             letters.append(name[0])
 # loop over all the letters
 for i in range(len(letters)):
-    # print the letter, add space to the end instead of the newline
+    # print the letter, add space to the end instead of newline
     print(f"{letters[i]} –", end=" ")
     # loop over all the animals
     for j in range(len(animals)):
         # if the first letter is the same as our letter
         if letters[i] == animals[j][0]:
-            # test if the length is the longest
+            longest_word_len = len(animals[j])
+            longest_word = animals[j]
+            # test if the length of the animal is the longest
             if len(animals[j]) > longest_word_len:
                 longest_word_len = len(animals[j])
                 longest_word = animals[j]
